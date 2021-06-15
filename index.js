22e3a813130026e133bfc7394cc4bad5909d4ef5
steven chen <schen0096@gmail.com>
2021-06-15 05:58:03 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function writeCards(names, event) {
+    let emptyArray = [];
+    for (let i=0; i < names.length; i++){
+        emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return emptyArray;
+}
+
+writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+
+function countDown(input) {
+    let i = 0
+    while (input >= 0){
+        console.log(input--)
+    }
+}
+
+countDown(10)
